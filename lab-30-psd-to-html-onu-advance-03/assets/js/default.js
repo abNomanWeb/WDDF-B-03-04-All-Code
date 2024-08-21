ef35a780775832a022e5314f52afea5427fa4162
abnomanweb <abnoman.excel@gmail.com>
2024-08-21 15:51:02 +0600

    Lab 30 Code

---
+++ lab-30-psd-to-html-onu-advance-03/assets/js/default.js
@@ -0,0 +1,10 @@
+// navbar scroll background change code start
+const navbar = document.querySelector('#navbar');
+window.onscroll = () => {
+    if (window.scrollY > 100) {
+        navbar.classList.add('nav-active');
+    } else {
+        navbar.classList.remove('nav-active');
+    }
+};
+// navbar scroll background change code end
