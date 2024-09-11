728913b4f6b1b65335010fcd922984c37b6b880d
abnomanweb <abnoman.excel@gmail.com>
2024-09-11 20:14:50 +0600

    Lab 39 Code

---
+++ lab-39-javascript-variable-dataType-operators/script.js
@@ -0,0 +1,30 @@
+// prompt("What is your name:");
+
+// variables or identifiers declaration way..
+
+// Names can contain letters, digits, underscores, and dollar signs.
+let hello = 1;
+let hello1 = 1;
+let hello_1 = 1;
+let $hello_1 =1;
+let hellobangladesh = 1;
+let helloNewBangladesh = 1;
+let HelloBangladesh = 1;
+let Hello_Bangladesh = 1;
+// let 1Bangladesh = 1; // not valid declaration
+
+// var x = 1;
+// var x = 2;
+
+// let x = 1;
+// x = 2;
+
+// let x = 5;
+// x--;
+// x++;
+
+let z = x;
+
+alert(x);
+
+// let var = 100; //// not valid declaration
