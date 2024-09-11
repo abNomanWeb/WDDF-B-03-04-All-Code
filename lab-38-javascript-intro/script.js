400e7a4bd156d81f0cbe00460e01e1b5d2601de7
abnomanweb <abnoman.excel@gmail.com>
2024-09-11 20:14:21 +0600

    Lab 38 Code

---
+++ lab-38-javascript-intro/script.js
@@ -0,0 +1,9 @@
+prompt("What is your name:");
+
+// window.alert("Hello new\'s Bangladesh.");
+
+// alert('Hello Bangladesh 2.0.');
+// document.write('I am javaScript Learner');
+// console.log('I am a output. Please Find me..');
+// document.getElementById('yahoo').innerHTML = "Hello I am from Inner Part.";
+// document.getElementById('google').innerText = "Hello I am from Text Part.";
