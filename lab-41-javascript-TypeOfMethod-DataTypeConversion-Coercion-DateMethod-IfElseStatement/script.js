700a82853040432b072983a814e779beb5121c10
abnomanweb <abnoman.excel@gmail.com>
2024-09-11 20:20:45 +0600

    Lab 41 Code

---
+++ lab-41-javascript-TypeOfMethod-DataTypeConversion-Coercion-DateMethod-IfElseStatement/script.js
@@ -0,0 +1,33 @@
+// let x = 100;
+// let y = 100;
+
+// let userInput = prompt("Input Your Number: ");
+
+// let userInput = new Date().getHours();
+
+// alert (userInput);
+
+// if(userInput<6){
+//     document.write('Good Morning');
+// }else if(userInput<=12){
+//     document.write('Good Day');
+// }else if(userInput<=18){
+//     document.write('Good Evening');
+// }else{
+//     document.write('Good Night');
+// }
+
+
+// let firstNumber = prompt("Input First Number: ");
+// let secondNumber = prompt("Input Second Number: ");
+
+// let result = Number(firstNumber) + Number(secondNumber);
+
+// document.write("Your Result Is: " + result);
+
+let userInput = prompt("Input Current Temperature: ");
+let result = 9/5*userInput+32;
+
+alert("Current Temperature is: " + result + " Degree Fahrenheit");
+
+document.write("Task is Complete.!");
