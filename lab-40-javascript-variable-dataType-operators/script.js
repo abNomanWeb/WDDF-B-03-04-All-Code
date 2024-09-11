db7c5bf1270b40447e99c6c218a6685b5f9351e9
abnomanweb <abnoman.excel@gmail.com>
2024-09-11 20:19:04 +0600

    Lab 40 Code

---
+++ lab-40-javascript-variable-dataType-operators/script.js
@@ -0,0 +1,16 @@
+// let x;
+// let y = '100';
+
+// if(x <= y){
+//     document.write('This condition is True');
+// }else{
+//     document.write('This condition is False');
+// }
+
+// let ternary = (x <= y) ? "This condition is True":"This condition is False";
+
+// alert(ternary);
+
+let x = ["Saab", "Volvo", "BMW"];
+
+alert(typeof(x));
